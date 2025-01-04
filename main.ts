10d0e15217baea8744a81cff9d351bac3ef9f7a4
Literou8860 <philemondefleurian@gmail.com>
2025-01-04 18:04:57 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-
+basic.forever(function () {
+    if (input.temperature() < 0) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.slide), music.PlaybackMode.UntilDone)
+    }
+})
+basic.forever(function () {
+    basic.showString("" + (input.temperature()))
+})
+basic.forever(function () {
+    if (input.temperature() > 10) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+    }
+})
+basic.forever(function () {
+    if (input.temperature() < 10) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+    }
+})
+basic.forever(function () {
+    if (input.temperature() < 5) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
+    }
+})
